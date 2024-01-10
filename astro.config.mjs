d8165f268f5d2ecce3abe7cbfb7966dd9851a290
Adam Robertson <admrbsn@gmail.com>
2024-01-10 11:55:29 -0700

    Added robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://readingrhythms.co',
+  integrations: [tailwind(), robotsTxt()],
   output: 'server',
   adapter: netlify(),
 });
