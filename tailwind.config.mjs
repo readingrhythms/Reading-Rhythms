834775a14997f38af1075ba099fb971b91cb61f3
Adam Robertson <admrbsn@gmail.com>
2024-01-22 12:14:34 -0700

    Adding new brooklyn page.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,15 +7,41 @@ export default {
     extend: {
       colors: {
         'dark-gray': '#0d0a0b',
+        'luma-gray': '#141517',
+        'luma-medium-gray': '#202325',
+        'luma-light-gray': '#1c1e20',
         'spotify-green': '#1bd760',
         jean: '#2d4971',
         beaber: '#8b7867',
         rust: '#b55336',
         'dark-purple': '#2f131f',
       },
+      backgroundImage: {
+        'custom-gradient':
+          'linear-gradient(rgba(66, 144, 243, 0.2) 0%, rgba(239, 148, 225, 0.1) 50%, rgba(191, 124, 7, 0) 100%)',
+      },
       fontFamily: {
         serif: ['Prestige Elite', ...defaultTheme.fontFamily.serif],
         display: ['Ziclets', 'ui-sans-serif', 'system-ui'],
+        system: [
+          '-apple-system',
+          'system-ui',
+          'Inter',
+          'Roboto',
+          'Segoe UI',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+      width: {
+        '3/10': '30%',
+        '7/10': '70%',
       },
     },
   },
